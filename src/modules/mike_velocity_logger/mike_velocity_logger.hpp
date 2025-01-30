5029b16a39737811ab0c22c4a09616de929b608b
Mike Lyons <mike.lyons@9threesolutions.com>
2025-01-30 04:08:25 -0800

    update to handle axes changes and scaling factors

--- src/modules/mike_velocity_logger/mike_velocity_logger.hpp
+++ src/modules/mike_velocity_logger/mike_velocity_logger.hpp
@@ -29,7 +29,11 @@ private:
 	perf_counter_t	_loop_perf;
 	vehicle_imu_s _vehicle_imu{};
 	vehicle_status_s _vehicle_status{};
+
 	float vx = 0.0f, vy = 0.0f, vz = 0.0f;
+	float dt = 1000.0f; // Integration delta time in microseconds
+	float scaling_factor_imu = 1.0e6f; // Convert to correct units
+    	float imu_axes_directions[3] = {-1.0f, -1.0f, -1.0f}; // IMU installation direction of axes
 
 	void Run() override;
 	void integrate_accel_and_publish(const vehicle_imu_s &imu);
