d31164d576c693eaf073049ceee444adb4d4b947
Mike Lyons <mike.lyons@9threesolutions.com>
2025-01-30 03:21:55 -0800

    add new mike_velocity_logger

---
+++ src/modules/mike_velocity_logger/mike_velocity_logger.cpp
@@ -0,0 +1,91 @@
+#include "mike_velocity_logger.hpp"
+
+extern "C" __EXPORT int mike_velocity_logger_main(int argc, char *argv[]) {
+    return MikeVelocityLogger::main(argc, argv);
+}
+
+MikeVelocityLogger::MikeVelocityLogger():
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::lp_default),
+	_loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle"))
+{
+}
+
+MikeVelocityLogger::~MikeVelocityLogger()
+{
+	perf_free(_loop_perf);
+}
+
+void MikeVelocityLogger::integrate_accel_and_publish(const vehicle_imu_s &imu) {
+
+
+    float dt = imu.delta_velocity_dt * 1e-6f; // Convert microseconds to seconds
+    vx += imu.delta_velocity[0] * dt;
+    vy += imu.delta_velocity[1] * dt;
+    vz += imu.delta_velocity[2] * dt;
+
+    integrated_accel_s msg{};
+    msg.timestamp = hrt_absolute_time();
+    msg.vx = vx;
+    msg.vy = vy;
+    msg.vz = vz;
+
+    _integrated_accel_pub.publish(msg);
+
+}
+
+void MikeVelocityLogger::Run() {
+
+    if (should_exit()) {
+		ScheduleClear();
+		return;
+	}
+
+	perf_begin(_loop_perf);
+
+    if (_vehicle_imu_sub.update(&_vehicle_imu)) {
+        _vehicle_status_sub.update(&_vehicle_status);
+
+        if (_vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_ARMED) {
+            integrate_accel_and_publish(_vehicle_imu);
+        }
+    }
+    perf_end(_loop_perf);
+    //ScheduleNow();
+    ScheduleDelayed(100000); // Run at 10 Hz
+}
+
+int MikeVelocityLogger::task_spawn(int argc, char *argv[]) {
+    MikeVelocityLogger *instance = new MikeVelocityLogger();
+
+    if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+        instance->ScheduleNow();
+	}
+
+    return PX4_OK;
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+int MikeVelocityLogger::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int MikeVelocityLogger::print_usage(const char *reason)
+{
+    if (reason) {
+        PX4_WARN("%s", reason);
+    }
+    PX4_INFO("Usage: mike_velocity_logger {start|stop|status}");
+    return 0;
+}
+
+
+
+
---
+++ src/modules/mike_velocity_logger/mike_velocity_logger.hpp
@@ -0,0 +1,36 @@
+#include <drivers/drv_hrt.h>
+#include <lib/perf/perf_counter.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/topics/vehicle_imu.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/integrated_accel.h>
+
+using matrix::Vector3f;
+using uORB::SubscriptionData;
+using namespace time_literals;
+
+class MikeVelocityLogger: public ModuleBase<MikeVelocityLogger>, public px4::ScheduledWorkItem
+{
+public:
+	MikeVelocityLogger();
+	~MikeVelocityLogger();
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+
+private:
+	uORB::Publication<integrated_accel_s> _integrated_accel_pub{ORB_ID(integrated_accel)};
+	uORB::Subscription _vehicle_imu_sub{ORB_ID(vehicle_imu)};
+	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+	perf_counter_t	_loop_perf;
+	vehicle_imu_s _vehicle_imu{};
+	vehicle_status_s _vehicle_status{};
+	float vx = 0.0f, vy = 0.0f, vz = 0.0f;
+
+	void Run() override;
+	void integrate_accel_and_publish(const vehicle_imu_s &imu);
+};
